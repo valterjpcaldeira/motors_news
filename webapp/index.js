b99bdc8a9ac3c9540a4567cf0d1933ac152d3445
davidfernandes <davidalexandremf15@hotmail.com>
2018-05-10 20:27:27 +0100

    I made html formula E

--- webapp/index.js
+++ webapp/index.js
@@ -30,6 +30,10 @@ app.get('/formulatruck', function (req, res, next) {
 	res.sendFile(path.join(__dirname + '/private/formulatruck.html'));
 });
 
+app.get('/formulae', function (req, res, next) {
+	res.sendFile(path.join(__dirname + '/private/formulae.html'));
+});
+
 
 
 http.listen(80, function(){
